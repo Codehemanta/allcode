c66c5ba293d83fbcac5b9aab7743097961777bc5
Hemanta_Gayen <69314427+Codehemanta@users.noreply.github.com>
2020-10-05 21:05:03 +0530

    Add files via upload

---
+++ auto friend request fb 5oct2020.js
@@ -0,0 +1,9 @@
+var num = 4;
+var secend = 1000;
+function request() {
+	document.getElementsByClassName("_3-8_ img sp_Ruuco-0Gyqq sx_cfa2ef")[num].click();
+	num +=1
+
+	
+}
+setInterval(request,secend);
